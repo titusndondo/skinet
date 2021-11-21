bba1231461aa10ae8cec68649d77c201a8db6136
Titus Ndondo <titus@bidvestalice.com>
2021-11-21 19:48:57 +0200

    End of section 5

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
